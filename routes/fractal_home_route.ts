66f6abb8f98ef169dd719b8f042160025934c8e7
Acellam Guy <acellam.guy@gmail.com>
2024-08-18 14:56:47 +0300

    refactor: show welcome message on root

--- routes/fractal_home_route.ts
+++ routes/fractal_home_route.ts
@@ -1,13 +1,14 @@
 import { Application, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-
+import { version, description } from "../package.json";
 export class FractalHome {
   public constructor(app: Application) {
-    app.route(process.env.API_BASE as string)
+    app.route("/*")
       // GET endpoint
       .get((_req: Request | any, res: Response) => res.status(StatusCodes.OK).json({
-        message: "Fractal API",
-        version: "1.0.0" // TODO: Read version from package.json
+        message: "Welcome to Fractal Js!",
+        description,
+        version
       }));
     // check health of the app
     app.route(`${process.env.API_BASE}_health`)
